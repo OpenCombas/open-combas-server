97143cdefbd6ae504bd15ba4485df2ce6c82beb4
Łukasz Milewski <milewski.lukasz.91@gmail.com>
2025-05-30 11:51:25 +0200

    [feat] declarative server setup

--- src/config.go
+++ src/config.go
@@ -1,20 +1,31 @@
 package main
 
 type ServerConfig struct {
-	WorldPort        int    `json:"WorldPort" env:"WorldPort"`
-	WorldOldPort     int    `json:"WorldOtherPort" env:"WorldOtherPort"`
-	ServerStatusPort int    `json:"ServerStatusPort" env:"ServerStatusPort"`
-	ListeningAddress string `json:"ListeningAddress" env:"ListeningAddress"`
-	BufferSize       int    `json:"BufferSize" env:"BufferSize"`
+	ServerStatusPort int                `json:"ServerStatusPort" env:"ServerStatusPort"`
+	ListeningAddress string             `json:"ListeningAddress" env:"ListeningAddress"`
+	BufferSize       int                `json:"BufferSize" env:"BufferSize"`
+	EchoingServers   []EchoServerConfig `json:"EchoingServers" env:"EchoingServers"`
+}
+type EchoServerConfig struct {
+	Label string
+	Port  int
 }
 
 func LoadConfig() ServerConfig {
 	cfg := ServerConfig{
-		WorldPort:        1215,
-		WorldOldPort:     1255,
 		ServerStatusPort: 1207,
 		ListeningAddress: "0.0.0.0",
 		BufferSize:       4000,
+		EchoingServers: []EchoServerConfig{
+			{
+				Label: "WORLD",
+				Port:  1215,
+			},
+			{
+				Label: "WORLD_OLD",
+				Port:  1255,
+			},
+		},
 	}
 	//todo: parse config file, for now it is hardcoded
 
