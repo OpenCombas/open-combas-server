5ebd242a529367a3e0f633d4e9c3742fce0a3e25
imaginebeingatcomputers <ibac@lxcz.info>
2026-07-15 18:19:54 -0500

    chore: letsencrypt cert mounting for docker-compose nginx proxy

---
+++ prepare_certs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###############################################################################################################
+# Allows mounting letsencrypt generate ssl certificates inside nginx container by granting your current user 
+# acl read access to your .env specified letsencrypt live+archive directory
+# there are better ways to do this....
+# use at your own risk. 
+###############################################################################################################
+source .env
+setfacl -R -m u:${whoami}:rx /etc/letsencrypt/live/${HOSTNAME} /etc/letsencrypt/archive/${HOSTNAME} /etc/letsencrypt/ssl-dhparams.pem /etc/letsencrypt/options-ssl-nginx.conf
